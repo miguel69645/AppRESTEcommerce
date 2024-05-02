d39f0c9c35b76182ec3f55c00f59ce931b00f143
miguel69645 <miancarrilloal@ittepic.edu.mx>
2024-05-01 23:26:11 -0700

    Api de labels e institutos completas

--- src/api/pwa2/models/labels.jsx
+++ src/api/pwa2/models/labels.jsx
@@ -2,15 +2,17 @@ import * as mongoose from "mongoose";
 
 // Etiquetas
 
+const valorSchema = new mongoose.Schema({
+  IdValorOK: { type: String, required: true },
+  Valor: { type: String, required: true },
+});
+
 const etiquetasSchema = new mongoose.Schema({
   IdEtiquetaOK: { type: String, required: true },
   NombreEtiqueta: { type: String, required: true },
   Descripcion: { type: String, required: false },
   Alias: { type: String, required: false },
+  valores: { type: [valorSchema], required: false }, // Agregar una propiedad 'valores'
 });
 
-export default mongoose.model(
-  "cat_labels",
-  etiquetasSchema,
-  "cat_labels"
-);
+export default mongoose.model("cat_labels", etiquetasSchema, "cat_labels");

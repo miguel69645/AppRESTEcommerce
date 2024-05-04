f5917119cc2e8158d0e535ad2086cb50f3e89800
miguel69645 <miancarrilloal@ittepic.edu.mx>
2024-05-04 13:52:46 -0700

    carpeta dist

--- src/app.jsx
+++ src/app.jsx
@@ -20,6 +20,7 @@ app.set('port', config.PORT);
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static('dist'));
 app.use(express.urlencoded({ extended: false }));
 //FIC: Routes
 const api = config.API_URL;

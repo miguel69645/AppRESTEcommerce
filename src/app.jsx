350418f76e3f0e880cc359aed1243e020c946d1c
miguel69645 <miancarrilloal@ittepic.edu.mx>
2024-05-20 00:48:21 -0700

    Manu

--- src/app.jsx
+++ src/app.jsx
@@ -13,12 +13,6 @@ import routeAPICOM from "./api/v1/routes/index";
 import routeAPIPWA from "./api/pwa/routes/index";
 import routeAPIPWA2 from "./api/pwa2/routes/index";
 import routeAPIPWA3 from "./api/pwa3/routes/index";
-import routeAPIPWA4 from "./api/pwa4/routes/index";
-// import routeAPIPWA5 from "./api/pwa5/routes/index";
-// import routeAPIPWA6 from "./api/pwa6/routes/index";
-// import routeAPIPWA7 from "./api/pwa7/routes/index";
-// import routeAPIPWA8 from "./api/pwa8/routes/index";
-// import routeAPIPWA9 from "./api/pwa9/routes/index";
 //FIC: Declaramos la variable app igualandola a express
 const app = express();
 //FIC: Settings
@@ -46,12 +40,6 @@ routeAPICOM(app);
 routeAPIPWA(app);
 routeAPIPWA2(app);
 routeAPIPWA3(app);
-routeAPIPWA4(app);
-// routeAPIPWA5(app);
-// routeAPIPWA6(app);
-// routeAPIPWA7(app);
-// routeAPIPWA8(app);
-// routeAPIPWA9(app);
 // Swagger Docs
 // Middleware para el manejo de errores
 // Export App
--- src/config/config.jsx
+++ src/config/config.jsx
@@ -5,7 +5,7 @@ export default {
     PORT: process.env.PORT || 'NO ENCONTRE PORT',
     API_URL: process.env.API_URL || '/api',
     // CONNECTION_STRING: process.env.CONNECTION_STRING || 'mongodb://localhost:27017/?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000',
-    CONNECTION_STRING: process.env.CONNECTION_STRING || 'mongodb+srv://miguel:Tec12345678@innedutec.rsxjfcd.mongodb.net/',
+    CONNECTION_STRING: process.env.CONNECTION_STRING || 'mongodb+srv://manu:Tec12345678@cluster0.tvgq5vw.mongodb.net/',
     DATABASE: process.env.DATABASE || 'db_ecommerce', 
     DB_USER: process.env.DB_USER || 'AdminUser', 
     DB_PASSWORD: process.env.DB_PASSWORD || 'florecitarockera',
